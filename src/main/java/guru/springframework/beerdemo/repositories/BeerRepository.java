ca8196567295a8bc2b736ff77af9f3a2ffa025bd
aleVeD <aldmove@gmail.com>
2020-07-14 12:52:21 -0400

    adding beer repository

---
+++ src/main/java/guru/springframework/beerdemo/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.beerdemo.repositories;
+
+import guru.springframework.beerdemo.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
